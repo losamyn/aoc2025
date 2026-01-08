af50bf3e92ce301e5cbde446e233a8fd869ca3e6
Louis Samyn <louis.samyn@telenet.be>
2026-01-08 20:34:21 +0100

    Solution for day 1

--- 01.py
+++ 01.py
@@ -5,18 +5,54 @@ Usage:
 uv run adventofcode run 01.py
 """
 
-inp = """your input"""
+inp = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
+
+dial_start = 50
+dial_size = 100
+
 part1_asserts = [
-    (inp, None),
+    (inp, 3),
 ]
 part2_asserts = [
-    (inp, None),
+    (inp, 6),
 ]
 
 
-def part1(inp: str) -> str | int | None:
-    return None
+def part1(inp: str) -> int:
+    dial = dial_start
+    result = 0
+
+    inp = inp.replace("L", "-").replace("R", "")
+    for amount in map(int, inp.split()):
+        dial = (dial + amount) % dial_size
+        if dial == 0:
+            result += 1
+    return result
 
 
-def part2(inp: str) -> str | int | None:
-    return None
+def part2(inp: str) -> int:
+    dial = dial_start
+    result = 0
+
+    inp = inp.replace("L", "-").replace("R", "")
+    for amount in map(int, inp.split()):
+        if amount < 0:
+            # Going to the left requires shifting by 1 to properly count zeroes
+            new_dial = (dial - 1) % dial_size + amount
+            dial = (new_dial + 1) % dial_size
+            result += abs(new_dial // dial_size)
+        else:
+            new_dial = dial + amount
+            dial = new_dial % dial_size
+            result += abs(new_dial // dial_size)
+
+    return result
---
+++ README.md
@@ -0,0 +1,15 @@
+
+## Benchmark Results
+
+<!-- BENCHMARK_RESULTS_START -->
+| Day | Status | Part 1 Time | Part 2 Time | Total Time | 
+|----:|:------:|------------:|------------:|-----------:| 
+| 01 | ✅ | 0.92ms 🟢 | 1.31ms 🟢 | 2.23ms 🟢 |
+| **Total** | | 0.92ms 🟢 | 1.31ms 🟢 | 2.23ms 🟢 |
+
+Legend:
+ * 🟢 < 100ms
+ * 🟡 100ms - 1s
+ * 🔴 > 1s
+ * ⚪ Not included in total
+<!-- BENCHMARK_RESULTS_END -->
