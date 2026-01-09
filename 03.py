caec93e0a9d96e079b25e81ac8e41f983788d988
Louis Samyn <louis.samyn@telenet.be>
2026-01-09 17:14:56 +0100

    Solution for day 3

--- 03.py
+++ 03.py
@@ -5,18 +5,40 @@ Usage:
 uv run adventofcode run 03.py
 """
 
-inp = """your input"""
+inp = """987654321111111
+811111111111119
+234234234234278
+818181911112111
+"""
 part1_asserts = [
-    (inp, None),
+    (inp, 357),
 ]
 part2_asserts = [
-    (inp, None),
+    (inp, 3121910778619),
 ]
 
 
-def part1(inp: str) -> str | int | None:
-    return None
+def part1(inp: str) -> int:
+    result = 0
+    for bank in inp.split():
+        # string characters can be compared, only convert to int at end
+        first = max(bank[:-1])
+        bank_rest = bank[bank.find(first) + 1 :]
+        joltage = first + max(bank_rest)
+        result += int(joltage)
+
+    return result
 
 
-def part2(inp: str) -> str | int | None:
-    return None
+def part2(inp: str) -> int:
+    result = 0
+    for bank in inp.split():
+        joltage = ""
+        for i_bat in range(-11, 1):
+            possible_bats = bank[:i_bat] if i_bat < 0 else bank
+            next_bat = max(possible_bats)
+            bank = bank[bank.find(next_bat) + 1 :]
+            joltage = joltage + next_bat
+        result += int(joltage)
+
+    return result
