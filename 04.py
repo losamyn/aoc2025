d2047169714f9c014579955bf43e00ef0e3b7144
Louis Samyn <louis.samyn@telenet.be>
2026-01-09 18:25:28 +0100

    Solution for day 4 part 1

--- 04.py
+++ 04.py
@@ -5,18 +5,48 @@ Usage:
 uv run adventofcode run 04.py
 """
 
-inp = """your input"""
+inp = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
 part1_asserts = [
-    (inp, None),
+    (inp, 13),
 ]
 part2_asserts = [
     (inp, None),
 ]
 
 
-def part1(inp: str) -> str | int | None:
-    return None
+def pad(inp):
+    padded = list(map(lambda line: "." + line + ".", inp))
+    horizontal_padding = ["." * len(padded[0])]
+    return horizontal_padding + padded + horizontal_padding
 
 
-def part2(inp: str) -> str | int | None:
+def is_accessible(inp, x, y) -> bool:
+    result = -(inp[y][x] == "@")  # -True is equal to -1
+    for ix in range(-1, 2):
+        for iy in range(-1, 2):
+            result += inp[y + iy][x + ix] == "@"
+    return result < 4
+
+
+def part1(inp: str) -> int:
+    inp = pad(inp.split())
+    result = 0
+    for x in range(1, len(inp[0]) - 1):
+        for y in range(1, len(inp) - 1):
+            if inp[y][x] == "@":
+                result += is_accessible(inp, x, y)
+    return result
+
+
+def part2(inp: str) -> int:
     return None
