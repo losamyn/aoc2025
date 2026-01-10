1e9eb39f47cc440cd009d2475568332f31021d26
Louis Samyn <louis.samyn@telenet.be>
2026-01-10 19:17:00 +0100

    Solution for day 5 part 1

--- 05.py
+++ 05.py
@@ -5,18 +5,39 @@ Usage:
 uv run adventofcode run 05.py
 """
 
-inp = """your input"""
+inp = """3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+"""
 part1_asserts = [
-    (inp, None),
+    (inp, 3),
 ]
 part2_asserts = [
     (inp, None),
 ]
 
 
-def part1(inp: str) -> str | int | None:
-    return None
+def part1(inp: str) -> int:
+    # Can probably be optimized by compiling an ordered, non-overlapping ranges list
+    ranges, ids = map(str.split, inp.split("\n\n"))
+    ranges = list(map(lambda r: tuple(map(int, r.split("-"))), ranges))
+    ids = map(int, ids)
+    result = 0
+    for id in ids:
+        for lower, upper in ranges:
+            if id >= lower and id <= upper:
+                result += 1
+                break
+    return result
 
 
-def part2(inp: str) -> str | int | None:
+def part2(inp: str) -> int:
     return None
