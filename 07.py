2ab6b08702b59519d9e4ab927833deb09d38af8c
Louis Samyn <louis.samyn@telenet.be>
2026-01-13 19:22:42 +0100

    Solution for day 7

--- 07.py
+++ 07.py
@@ -5,18 +5,57 @@ Usage:
 uv run adventofcode run 07.py
 """
 
-inp = """your input"""
+inp = """.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+..............."""
+
 part1_asserts = [
-    (inp, None),
+    (inp, 21),
 ]
 part2_asserts = [
-    (inp, None),
+    (inp, 40),
 ]
 
 
-def part1(inp: str) -> str | int | None:
-    return None
+def part1(inp: str) -> int:
+    lines = [list(line) for line in inp.split("\n")]
+    result = 0
+    for i in range(1, len(lines)):
+        for j in range(len(lines[i])):
+            if lines[i - 1][j] == "|" or lines[i - 1][j] == "S":
+                if lines[i][j] == "^":
+                    result += 1
+                    lines[i][j - 1] = "|"
+                    lines[i][j + 1] = "|"
+                elif lines[i][j] == ".":
+                    lines[i][j] = "|"
+    return result
 
 
-def part2(inp: str) -> str | int | None:
-    return None
+def part2(inp: str) -> int:
+    inp = inp.replace(".", "0").replace("S", "1")
+    lines = [list(line) for line in inp.split("\n")]
+    lines = [[-1 if c == "^" else int(c) for c in line] for line in lines]
+
+    for i in range(1, len(lines)):
+        for j in range(len(lines[i])):
+            if lines[i - 1][j] > 0:
+                if lines[i][j] < 0:
+                    lines[i][j - 1] += lines[i - 1][j]
+                    lines[i][j + 1] += lines[i - 1][j]
+                else:
+                    lines[i][j] += lines[i - 1][j]
+    return sum(lines[-1])
