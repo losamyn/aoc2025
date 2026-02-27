9ee5e60be9efda8ac3b5ec54a2b815500eb20549
Louis Samyn <louis.samyn@telenet.be>
2026-02-27 14:34:36 +0100

    Attempt at solving day 10 part 2 (not working)

--- 10.py
+++ 10.py
@@ -11,13 +11,15 @@ inp = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 """
 inp2 = """[.#...#] (2,4) (3,4) (0,2,3,5) (0,1,2,3) (1,3,4,5) {26,32,28,47,23,20}
 """
+inp3 = """[#...#####.] (0,1,4,6,8,9) (1,2,3,5,6,8,9) (0,2,3,4,5,6,8) (0,4,5,6,7) (1,2,9) (2,5,9) (2,4,6,7) (0,3,4,6,9) {52,30,67,53,55,67,72,17,36,68}
+"""
 part1_asserts = [
     (inp, 7),
 ]
 part2_asserts = [
     (inp, 33),
     (inp2, 49),
-    (inp2, 0),
+    (inp3, 102),
 ]
 
 
@@ -57,39 +59,54 @@ def calculate_steps(goal, buttons):
 
 
 def calculate_steps_two(goal, buttons):
-    # Approach: get state to all zeroes and keep track of which buttons are used for it
-    state = list(goal)
-    steps = {button: 0 for button in buttons}
-    sorted_buttons = sorted(buttons, key=len, reverse=True)
-    while len(sorted_buttons):
-        # Update available buttons
-        buttons_by_counter = {counter: [] for counter in range(len(state))}
-        for button in list(sorted_buttons):
-            if all([state[c] > 0 for c in button]):
-                for c in button:
-                    buttons_by_counter[c].append(button)
-            else:
-                sorted_buttons.remove(button)
-        if not len(sorted_buttons):
-            break
-        # Select next button
-        next_button = None
-        for buttons_for_c in buttons_by_counter.values():
-            if len(buttons_for_c) == 1:
-                next_button = buttons_for_c[0]
+    # Could not figure this one out myself, using the approach described in following post:
+    # https://old.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory
+    aug_matrix = [
+        [int(row in button) for button in buttons] + [joltage]
+        for row, joltage in enumerate(goal)
+    ]
+    rows_n = len(aug_matrix)
+    columns_n = len(aug_matrix[0])
+    # RREF -- making the assumption that all divisions will be whole integers
+    for current_row in range(rows_n):
+        # Find next non-zero row, swap it to current row
+        current_column = columns_n
+        to_swap = current_row
+        for row_i in range(current_row, rows_n):
+            first_one = next((i for i, x in enumerate(aug_matrix[row_i]) if x), None)
+            if first_one is not None and first_one < current_column:
+                current_column = first_one
+                to_swap = row_i
+            if current_column == current_row:
                 break
-        else:
-            next_button = sorted_buttons[0]
-        # Press the button as many times as possible
-        available_presses = min([state[c] for c in next_button])
-        for c in next_button:
-            state[c] -= available_presses
-        steps[next_button] += available_presses
-    if any(state):
-        print("Joltages not complete for:")
-        print(goal, "-- Final state:", state, steps, "-- sum:", sum(steps.values()))
-        # return None
-    return sum(steps.values())
+        if current_column == columns_n:
+            break
+        if to_swap != current_row:
+            # Swap rows
+            tmp = aug_matrix[to_swap]
+            aug_matrix[to_swap] = aug_matrix[current_row]
+            aug_matrix[current_row] = tmp
+        # Scale current row
+        scale = aug_matrix[current_row][current_column]
+        for i in range(current_column, columns_n):
+            if aug_matrix[current_row][i] % scale:
+                print(
+                    "RREF warning: cant divide ",
+                    aug_matrix[current_row][i],
+                    " by ",
+                    scale,
+                    " --- ",
+                    goal,
+                )
+            aug_matrix[current_row][i] = aug_matrix[current_row][i] / scale
+        # Subtract current row from other rows
+        for i in range(rows_n):
+            scale = aug_matrix[i][current_column]
+            if i == current_row or scale == 0:
+                continue
+            for j in range(current_column, columns_n):
+                aug_matrix[i][j] -= aug_matrix[current_row][j] * scale
+    return int(sum([row[-1] for row in aug_matrix]))
 
 
 def part1(inp: str) -> int:
