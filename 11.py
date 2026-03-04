c928e8c74cf531342557cd0899e9cce39085bc34
Louis Samyn <louis.samyn@telenet.be>
2026-03-04 12:35:36 +0100

    Solution for day 11

--- 11.py
+++ 11.py
@@ -5,18 +5,72 @@ Usage:
 uv run adventofcode run 11.py
 """
 
-inp = """your input"""
+from functools import cache
+
+inp = """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+"""
+inp2 = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+"""
 part1_asserts = [
-    (inp, None),
+    (inp, 5),
 ]
 part2_asserts = [
-    (inp, None),
+    (inp2, 2),
 ]
 
 
-def part1(inp: str) -> str | int | None:
-    return None
+def part1(inp: str) -> int:
+    graph = {
+        node: connections.split()
+        for node, connections in [line.split(": ") for line in inp.strip().split("\n")]
+    }
+
+    @cache
+    def count_paths(start_node: str) -> int:
+        if start_node == "out":
+            return 1
+        return sum(count_paths(node) for node in graph[start_node])
+
+    return count_paths("you")
 
 
-def part2(inp: str) -> str | int | None:
-    return None
+def part2(inp: str) -> int:
+    graph = {
+        node: connections.split()
+        for node, connections in [line.split(": ") for line in inp.strip().split("\n")]
+    }
+
+    @cache
+    def count_paths(start_node: str, visited_dac=False, visited_fft=False) -> int:
+        if start_node == "out":
+            return visited_dac and visited_fft
+        if start_node == "dac":
+            visited_dac = True
+        elif start_node == "fft":
+            visited_fft = True
+        return sum(
+            count_paths(node, visited_dac, visited_fft) for node in graph[start_node]
+        )
+
+    return count_paths("svr")
